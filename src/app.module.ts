b3b625763e1d4413c3ed1812b34503986acf90bd
Fahad Hussain <fahd__hussain@hotmail.com>
2022-04-10 01:54:32 +0500

    Response Interceptor to map response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './module/auth/auth.module';
 import { UserModule } from './module/user/user.module';
 import { HttpErrorFilter } from './shared/filter/http-error.filter';
 import { AuthGuard } from './shared/guard/auth.guard';
+import { ResponseInterceptor } from './shared/interceptor/response.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AuthModule, UserModule],
@@ -16,6 +17,10 @@ import { AuthGuard } from './shared/guard/auth.guard';
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
     },
   ],
 })
